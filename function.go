48f59863ca31f1b4819fbb989eac361b8e6fd970
Rohit Raj <rohitrjsinha@gmail.com>
2018-08-22 15:57:28 +0530

    master: function invocation

--- function.go
+++ function.go
@@ -5,6 +5,12 @@ import "fmt"
 func printer () {
 	fmt.Println("Hello, I am called from main")
 }
+
+//the main method is the starting point of the execution.
+/**
+ * Main method is similar to main method of C/C++
+ * Comments are also same as C/C++
+ */
 func main() {
 	printer()
 }
